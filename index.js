78c63bb0a073362767f569a14381f9c969b22975
unknown <samson.theodor3699@gmail.com>
2022-09-17 17:15:15 +0300

    Add mongoose and DB

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/toDoListDB");
 
 const app = express();
 
@@ -8,25 +11,55 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-var items = [];
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Mananca"
+})
+
+const item2 = new Item({
+    name: "Dus"
+})
+
+const item3 = new Item({
+    name: "Grind"
+})
+
+// const defaultItems = [item1, item2, item3];
+
+// Item.insertMany(defaultItems, function(err){
+//     if (err){
+//         console.log(err);
+//     } else{
+//         console.log("Succesfully added default items");
+//     }
+// })
 
 app.get("/", function(req, res){
-    const today = new Date();
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    var day = today.toLocaleDateString("ro-RO", options);
-    res.render("list", {kindOfDay: day, itemsList: items});
-    console.log(today);
-    console.log(day);
+    Item.find({}, function(err, foundItems){
+        console.log(foundItems);
+        res.render("list", {kindOfDay: "Today", itemsList: foundItems});
+    })
 })
 
 app.post("/", function(req, res){
     var newItem = req.body.nextItem;
-    items.push(newItem);
+    Item.insertMany({name: newItem})
     res.redirect("/");
+})
+
+app.post("/delete", function(req, res){
+    var deleteItemID = req.body.deletedItem;
+    Item.findByIdAndRemove(deleteItemID, function(err){
+        if (!err){
+            console.log("Succesfully deleted the item " + deleteItemID);
+            res.redirect("/");
+        }
+    });
 })
 
 app.listen(3000, function(){
